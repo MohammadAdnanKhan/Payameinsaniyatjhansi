011abe398ef86b6d8cac5228afb46de3693a346e
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2026-03-20 02:39:03 +0530

    TBT reduction trail

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import Hero from "@/components/hero/hero";
-import Majorprojects from "@/components/additional/majorprojects";
 export default function HomePage() {
   
   return (
