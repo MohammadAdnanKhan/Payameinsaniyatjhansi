8d94249a4fab2902a9416a31117363bdb74fb7a3
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2026-03-24 14:51:10 +0530

    many thgs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,16 +3,17 @@ import ProjectTabs from "@/components/about/ProjectsSection/ProjectTabs";
 import Transparency from "@/components/about/Transparency";
 import CTA from "@/components/about/CTA";
 import StoryTabs from "@/components/about/story-tabs/StoryTabs";
+import VideoGallery from "@/components/videogallery/VideoGallery";
+
 export default function AboutPage() {
   return (
-    <main className="flex flex-col gap-28">
+    // Responsive gaps: 16 on mobile, 24 on tablet, 32 on desktop
+    <main className="flex flex-col gap-16 md:gap-24 lg:gap-32 overflow-hidden">
       <AboutHero />
       <StoryTabs />
-
-      <ProjectTabs/>
-
+      <ProjectTabs />
+      <VideoGallery />
       <Transparency />
-
       <CTA />
     </main>
   );
